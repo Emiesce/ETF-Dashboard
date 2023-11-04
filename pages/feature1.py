c3c273b40ab67cd894b92f3133fe3da5ed304842
Lazbert <85731679+Lazbert@users.noreply.github.com>
2023-11-04 14:56:18 +0800

    added parent categories

--- pages/feature1.py
+++ pages/feature1.py
@@ -2,11 +2,33 @@ import dash
 from dash import dcc, html
 import plotly.express as px
 import pandas as pd
+import json
 
 dash.register_page(__name__, path='/')
 
-layout = html.Div(
-    [
-        dcc.Markdown('# Empty Page 1')
-    ]
-)
+categories = None
+with open("./static/ETF_categories.json") as file:
+    categories = json.load(file)
+
+layout = html.Div([
+    
+    html.Div([
+        
+        html.Div([
+            html.Img(src="../assets/Icons/IconFilter.svg", className="w-[25px] h-[25px]"),
+            html.Span("Filter by Sector(s)", className="text-[18px] font-medium"),
+        ], className="flex gap-2 items-center"),
+        
+        html.Div([
+            
+            dcc.Checklist([
+                { "label": html.Span(category, className="ml-2"), "value": category }
+            ], labelClassName="!flex items-center", inputClassName="w-[20px] h-[20px] rounded-sm") for category in list(categories.keys())
+        
+        ], className="flex flex-col gap-2")
+    
+    ], className="p-4 flex flex-col gap-2 w-[20%] border border-gray-medium rounded-lg"),
+    
+    html.Div(["Output"], className="flex-grow border rounded-lg")
+
+], className="p-8 flex justify-center gap-12")
