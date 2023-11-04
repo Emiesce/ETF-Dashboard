c7a3e750daee77ba2a6a35914adf9480d55a9394
Lazbert <85731679+Lazbert@users.noreply.github.com>
2023-11-04 21:19:48 +0800

    feat: selected categories are shown

--- pages/feature1.py
+++ pages/feature1.py
@@ -1,5 +1,5 @@
 import dash
-from dash import dcc, html
+from dash import dcc, html, callback, Output, Input
 import plotly.express as px
 import pandas as pd
 import dash_mantine_components as dmc
@@ -31,7 +31,7 @@ layout = html.Div([
                         
                         dmc.AccordionPanel(
                             
-                            dcc.Checklist([
+                            dcc.Checklist(id=category, options=[
                                 {"label": html.Span(sub_category, className="ml-2"), "value": sub_category} for sub_category in categories[category]
                             ], labelClassName="my-2 ml-2 !flex items-center", inputClassName="min-w-[20px] min-h-[20px] rounded-sm")
                             
@@ -44,6 +44,34 @@ layout = html.Div([
         
     ], className="p-4 flex flex-col gap-4 w-[20%] border border-gray-medium rounded-lg"),
     
-    html.Div(["Output"], className="flex-grow border rounded-lg")
+    html.Div([
+        
+        html.Div([
+
+            html.Span("You have selected:", className="text-[18px] font-medium"),
+            
+            html.Div(id="selected_categories", className="flex flex-col gap-2")
+
+        ], className="flex-grow p-4 bg-gray-light rounded-lg"),
+        
+        html.Hr(className="border-b-2 border-bronze"),
+        
+        html.Div([], className="min-h-[90%] border border-gray-medium rounded-lg p-4")
+    
+    ], className="flex flex-grow flex-col gap-4")
 
 ], className="p-8 flex justify-center gap-12")
+
+@callback(Output("selected_categories", "children"),
+          [Input(category, "value") for category in list(categories.keys())])
+def get_selected_categories(*args):
+    selected_categories = {}
+    for ind, checklist in enumerate(args):
+        if checklist is not None and len(checklist):
+            parent = list(categories.keys())[ind]
+            selected_categories[parent] = checklist
+    #print(selected_categories)
+    
+    return [html.Div([
+        html.Span(f"{key} > {', '.join(val)}")
+    ]) for key, val in selected_categories.items()]
