b73cb36f5081e0750835a30f79057f866690e1fe
Lazbert <85731679+Lazbert@users.noreply.github.com>
2023-11-27 02:54:08 +0800

    renamed features on nav bar

--- dashboard.py
+++ dashboard.py
@@ -3,6 +3,8 @@ from dash import Dash, html, dcc
 import plotly.express as px
 import pandas as pd
 
+FEATURES = ["ETF Filter", "Competitor Analysis", "Recommendation"]
+
 tailwind_cdn = "https://cdn.tailwindcss.com"
 app = Dash(__name__,
            suppress_callback_exceptions=True,
@@ -28,7 +30,7 @@ app.layout = html.Div([
             ], className="flex gap-8 items-center font-medium"),
 
             html.Div([
-                dcc.Link(page['name'], id=page["name"], href=page['path'], className="px-4 py-2 border border-gray-medium rounded-lg focus:bg-aqua focus:text-white hover:bg-aqua hover:text-white", ) for page in dash.page_registry.values()
+                dcc.Link(FEATURES[ind], id=page["name"], href=page['path'], className="px-4 py-2 border border-gray-medium rounded-lg focus:bg-aqua focus:text-white hover:bg-aqua hover:text-white", ) for ind, page in enumerate(dash.page_registry.values())
             ], className="flex justify-center gap-4"),
         ], className="bg-gray-light h-[80px] px-4 flex justify-between items-center border-b border-gray-medium text-gray-medium"),
                 
